d7912c876d1c6d59768259b99b463466671962dd
Henrique <henriquedsneves>
2024-10-23 21:00:58 -0300

    commit

---
+++ colecoes/Pilha.java
@@ -0,0 +1,21 @@
+package colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+    public static void main(String[] args) {
+        Deque<String> livros = new ArrayDeque<>();
+        livros.add("O Pequeno Principe");
+        livros.push("Don quixote");
+
+        System.out.println(livros.peek());
+        System.out.println(livros.element());
+
+        System.out.println(livros.pop());
+        System.out.println(livros.poll());
+        for(String livro: livros){
+            System.out.println(livro);
+        }
+    }
+}
